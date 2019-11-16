62f3ec0ae040ebee43449fa4cccfb50f9697282f
Jure <jure.mrzek15@gmail.com>
2019-11-16 23:44:23 +0100

    Added circles on stationary points
    also made it so the program recognizes when you click on that circle. To do: make those stationary points draggable

--- javascript/script.js
+++ javascript/script.js
@@ -15,7 +15,7 @@ let finalPoint;
 pointsN.push(new Point(100,500));
 pointsN.push(new Point(400,200));
 pointsN.push(new Point(900,600));
-pointsN.push(new Point(1000,300));
+pointsN.push(new Point(1400,300));
 pointsN.push(new Point(700,100));
 pointsN.push(new Point(100,200));
 pointsN.push(new Point(150,100));
@@ -23,17 +23,21 @@ pointsN.push(new Point(150,100));
 let allColors = ["blue","magenta", "green"];
 let colorsIndex = 0;
 let colors = [];
-for(i=0; i<pointsN.length-2; i++){
-    colors.push(allColors[i%allColors.length]);
+for(i=0; i<pointsN.length; i++){
+    if(i<pointsN.length-2){
+        colors.push(allColors[i%allColors.length]);
+    }
 }
 
 mainLoop();
 function mainLoop(){
     ctx.clearRect(0, 0, canvas.width, canvas.height); //Clear the canvas
-    
+
     for(i=1; i<pointsN.length; i++){
         drawLine(pointsN[i-1], pointsN[i], "black"); //Draw stationary lines
+        drawCircle(pointsN[i-1].x, pointsN[i-1].y, "rgb(56,56,56)", 12);
     }
+    drawCircle(pointsN[pointsN.length-1].x, pointsN[pointsN.length-1].y, "black", 12);
     recursiveCalc(t, pointsN);
     
     pointsF.push(finalPoint);
@@ -56,7 +60,7 @@ function mainLoop(){
 function recursiveCalc(t, tab){ //recursive function to calculate all 
     if(tab.length<=2){
         finalPoint = getPointsA(t, tab)[0];
-        drawCircle(finalPoint.x, finalPoint.y, "red")
+        drawCircle(finalPoint.x, finalPoint.y, "red", 5);
         return;
     }
     else{
@@ -74,18 +78,18 @@ function recursiveCalc(t, tab){ //recursive function to calculate all
 function drawMultipleLines(points, color){
     for(i=1; i<points.length; i++){
         drawLine(points[i-1], points[i], color); //Draw moving lines
-        drawCircle(points[i].x, points[i].y, color); //Draw the points
+        drawCircle(points[i].x, points[i].y, color, 5); //Draw the points
         if(i==1){
-            drawCircle(points[i-1].x,points[i-1].y, color)
+            drawCircle(points[i-1].x,points[i-1].y, color, 5)
         }
     }
 }
 
-function drawCircle(x, y, color){
+function drawCircle(x, y, color, r){
     ctx.beginPath();
     ctx.strokeStyle = color;
     ctx.fillStyle = color;
-    ctx.arc(x, y, 5, 0, 2*Math.PI);
+    ctx.arc(x, y, r, 0, 2*Math.PI);
     ctx.stroke();
     ctx.fill();
 }
@@ -121,3 +125,19 @@ function Point(x, y) {
     this.x = x;
     this.y = y;
 }
+
+//Make it so you can move stationary points by dragging
+function intersectsCircle(point, circle, r) {
+    return Math.sqrt((point.x-circle.x) ** 2 + (point.y - circle.y) ** 2) <= r;
+}
+  
+canvas.addEventListener("click", function(event) {
+    let canvasProperties = canvas.getBoundingClientRect();
+    let mousePoint = new Point(event.clientX-canvasProperties.left, event.clientY-canvasProperties.top);
+    for(i=0; i<pointsN.length; i++){
+        if(intersectsCircle(mousePoint, pointsN[i], 12)) {
+            //canvas.style.cursor = "all-scroll";
+            console.log("clicked");
+        }
+    };
+  });
