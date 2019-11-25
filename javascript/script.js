ff3d28e9d063bfe507e0e79e609e59a9092d041d
Jure <jure.mrzek15@gmail.com>
2019-11-25 20:27:11 +0100

    Fixed two bugs
    Fixed the bug where you clicked on a point and it moved two at once, also fixed the bug where if you added multiple points on the same coordinates it would break the program. That was because the coordinates were strings. Also changed point generation, they now randomly generate. (plus changed the colors).

--- javascript/script.js
+++ javascript/script.js
@@ -2,9 +2,9 @@
 const canvas = document.getElementById("canvas");
 const canvasDiv = document.getElementById("canvasDiv");
 canvas.height = 800;
-canvas.width = 1500;
+canvas.width = 1200;
 const ctx = canvas.getContext("2d");
-ctx.lineWidth = 2;
+ctx.lineWidth = 3;
 //-----------------------------
 let pointsN = [];
 let pointsA = [];
@@ -13,31 +13,37 @@ let t = 0;
 let bezierAccuracy = 1000;
 let finalPoint;
 let mouse = new Point(0, 0);
+let finalLineWidth = 4;
+let midLineWidth = 2;
+let staticLineWidth = 5;
+let circleOutlineWidth = 8;
 
-let showLines = document.getElementById("showLines");
-let sliderX = document.getElementById("sliderX");
-let sliderY = document.getElementById("sliderY");
+const showLines = document.getElementById("showLines");
+const sliderX = document.getElementById("sliderX");
+const sliderY = document.getElementById("sliderY");
 let xPos = document.getElementById("xPos");
 let yPos = document.getElementById("yPos");
 let addPointButton = document.getElementById("addPointButton");
+let tempFinalPoint = new Point(0, 0);
 
-pointsN.push(new Point(100,500));
-pointsN.push(new Point(400,200));
-pointsN.push(new Point(900,600));
-pointsN.push(new Point(1400,300));
-pointsN.push(new Point(700,100));
-pointsN.push(new Point(100,200));
+sliderX.value = Math.random()*canvas.width;
+sliderY.value = Math.random()*canvas.height;
 
-let allColors = ["blue","magenta", "green"];
+for(let i=0; i<5; i+=1){
+    let tempPoint = new Point(Math.random()*(canvas.width-20)+20, Math.random()*(canvas.height-20)+20);
+    pointsN.push(tempPoint)
+}
+
+const allColors = ["#727a17", "#ffc500","#FF6347"];
+//const allColors = ["#2ecc71", "#e67e22","#9b59b6"];
 let colorsIndex = 0;
 let colors = [];
-let tempFinalPoint = new Point(0, 0);
-for(i=0; i<pointsN.length; i++){
+for(let i=0; i<pointsN.length; i++){
     if(i<pointsN.length-2){
         colors.push(allColors[i%allColors.length]);
     }
 }
-for(i=0; i<pointsN.length; i++){
+for(let i=0; i<pointsN.length; i++){
     pointsN.isDragging = false;
 }
 
@@ -45,19 +51,19 @@ mainLoop();
 function mainLoop(){
     ctx.clearRect(0, 0, canvas.width, canvas.height); //Clear the canvas
 
-    xPos.innerHTML = "X: "+sliderX.value;
+    xPos.innerHTML = "X: "+sliderX.value; //Change the html text on sliders
     yPos.innerHTML = "Y: "+sliderY.value;
 
     for(i=1; i<pointsN.length; i++){
-        drawLine(pointsN[i-1], pointsN[i], "black"); //Draw stationary lines
-        drawCircle(pointsN[i-1].x, pointsN[i-1].y, "black", 12);
+        drawLine(pointsN[i-1], pointsN[i], "black", staticLineWidth); //Draw stationary lines
+        drawCircle(pointsN[i-1].x, pointsN[i-1].y, "black", "#ecf0f1", 12);
     }
-    drawCircle(pointsN[pointsN.length-1].x, pointsN[pointsN.length-1].y, "black", 12);
+    drawCircle(pointsN[pointsN.length-1].x, pointsN[pointsN.length-1].y, "black", "#ecf0f1", 12);
     
     calcFinalPoint(t, pointsN); //calculates point that  draws the bezier
     pointsF.push(finalPoint);
     for(i=1; i<pointsF.length; i++){
-        drawLine(pointsF[i-1], pointsF[i], "red"); //Draw the bezier curve
+        drawLine(pointsF[i-1], pointsF[i], "red", finalLineWidth); //Draw the bezier curve
     }
 
     for(i=0; i<pointsN.length; i++){
@@ -77,20 +83,21 @@ function mainLoop(){
         t=0;
         pointsF = [];
     }
-
-    window.requestAnimationFrame(mainLoop);
+    setTimeout(function() {
+        window.requestAnimationFrame(mainLoop);
+      }, 1000 / 60);
 }
 
 function calcFinalPoint(t, tab){ //recursive function to calculate final point
     if(tab.length<=2){
         finalPoint = getPointsA(t, tab)[0];
-        drawCircle(finalPoint.x, finalPoint.y, "red", 5);
+        drawCircle(finalPoint.x, finalPoint.y, "red", "red", 3);
         return;
     }
     else{
         pointsA = getPointsA(t, tab);
         if(showLines.checked) {
-            drawMultipleLines(pointsA, colors[colorsIndex]); //Draws the lines on the screen
+            drawMultipleLines(pointsA, colors[colorsIndex], midLineWidth); //Draws the lines on the screen
             if(colorsIndex >= colors.length-1)
                 colorsIndex = 0;
             else
@@ -101,20 +108,21 @@ function calcFinalPoint(t, tab){ //recursive function to calculate final point
     }
 }
 
-function drawMultipleLines(points, color){
+function drawMultipleLines(points, color, lineWidth){
     for(i=1; i<points.length; i++){
-        drawLine(points[i-1], points[i], color); //Draw moving lines
-        drawCircle(points[i].x, points[i].y, color, 5); //Draw the points
+        drawLine(points[i-1], points[i], color, lineWidth); //Draw moving lines
+        drawCircle(points[i].x, points[i].y, color, color, 3); //Draw the points
         if(i==1){
-            drawCircle(points[i-1].x,points[i-1].y, color, 5)
+            drawCircle(points[i-1].x,points[i-1].y, color, color, 3)
         }
     }
 }
 
-function drawCircle(x, y, color, r){
+function drawCircle(x, y, colorStroke, colorFill, r){
+    ctx.lineWidth = circleOutlineWidth;
     ctx.beginPath();
-    ctx.strokeStyle = color;
-    ctx.fillStyle = color;
+    ctx.strokeStyle = colorStroke;
+    ctx.fillStyle = colorFill;
     ctx.arc(x, y, r, 0, 2*Math.PI);
     ctx.stroke();
     ctx.fill();
@@ -129,7 +137,8 @@ function getPointsA(t, tab){ //finds a midpoint for every line between points in
     return points;
 }
 
-function drawLine(point1, point2, color){
+function drawLine(point1, point2, color, lineWidth){
+    ctx.lineWidth = lineWidth;
     ctx.beginPath();
     ctx.strokeStyle = color;
     ctx.moveTo(point1.x, point1.y);
@@ -149,7 +158,7 @@ function Point(x, y) {
     this.y = y;
 }
 
-//Make it so you can move stationary points by dragging
+//Make it so you can move stationary points by dragging------------
 function intersectsCircle(point, circle, r) {
     return Math.sqrt((point.x-circle.x) ** 2 + (point.y - circle.y) ** 2) <= r;
 }
@@ -161,16 +170,13 @@ canvas.addEventListener("mouseup", function() {
 });
 
 canvas.addEventListener("mousedown", function() {
-    for(i=0; i<pointsN.length; i++){
-        if(intersectsCircle(mouse, pointsN[i], 20)) {
-            //canvas.style.cursor = "all-scroll";
-            pointsN[i].isDragging = true;
-        }
-    }
+    const intersecting = pointsN.filter(point => intersectsCircle(mouse, point, 20));
+    if(intersecting.length > 0)
+        intersecting[intersecting.length -1].isDragging = true;
 });
 
 canvasDiv.addEventListener("mousemove", function(event){
-    let canvasProperties = canvas.getBoundingClientRect();
+    const canvasProperties = canvas.getBoundingClientRect();
     mouse.x = event.clientX - canvasProperties.left;
     mouse.y = event.clientY-canvasProperties.top;
     if(mouse.x <= 0 || mouse.y <= 0 || mouse.x >= canvas.width || mouse.y >= canvas.height){
@@ -180,12 +186,23 @@ canvasDiv.addEventListener("mousemove", function(event){
     }
 });
 
+//Add a new point when we click the button
 addPointButton.addEventListener("click", function(){
-    pointsN.push(new Point(150,100));
+    pointsN.push(new Point(+sliderX.value,+sliderY.value));
+    console.log(pointsN);
+    colors = [];
+    for(i=0; i<pointsN.length; i++){
+        if(i<pointsN.length-2){
+            colors.push(allColors[i%allColors.length]);
+        }
+    }
+    sliderX.value = Math.random()*canvas.width;
+    sliderY.value = Math.random()*canvas.height;
+    calculateBezierAgain(t);
 });
 
 
-//Make it so when you change position of a point, the whole bezier redraws
+//Make it so when you change position of a point, the whole bezier redraws----------
 function calculateBezierAgain(t){
     let tempT = 0;
     pointsF = [];
